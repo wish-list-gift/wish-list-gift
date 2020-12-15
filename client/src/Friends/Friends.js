4579a3965c9bc58c969382632e1290d1f55edf41
Viviana Garcia <vivianandrea4@gmail.com>
2020-12-15 12:52:46 -0500

    added friends component bootstrap

--- client/src/Friends/Friends.js
+++ client/src/Friends/Friends.js
@@ -1,9 +1,86 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Friends = () => {
     return (
         <div>
+            <div className="content">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-4"><Link to="#custom-modal" className="btn btn-custom waves-effect waves-light mb-4" data-animation="fadein" data-plugin="custommodal" data-overlayspeed="200" data-overlaycolor="#36404a"><i className="mdi mdi-plus"></i> Add Member</Link>
+                        </div>
+                        {/* <!-- end col --> */}
+                    </div>
+                    {/* <!-- end row --> */}
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div className="text-center card-box">
+                                <div className="member-card pt-2 pb-2">
+                                    <div className="thumb-lg member-thumb mx-auto"><img src="https://bootdey.com/img/Content/avatar/avatar2.png" className="rounded-circle img-thumbnail" alt="profile-image" /></div>
+                                    <div className="">
+                                        <h4>Freddie J. Plourde</h4>
+                                        <p className="text-muted">@Founder <span>| </span><span><Link to="#" className="text-pink">websitename.com</Link></span></p>
+                                    </div>
+                                    <button type="button" className="btn btn-primary mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button>
+                                    <div className="mt-4">
+                                        <div className="row">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- end col --> */}
+                        <div className="col-lg-4">
+                            <div className="text-center card-box">
+                                <div className="member-card pt-2 pb-2">
+                                    <div className="thumb-lg member-thumb mx-auto"><img src="https://bootdey.com/img/Content/avatar/avatar3.png" className="rounded-circle img-thumbnail" alt="profile-image" />
+                                    </div>
+                                    <div className="">
+                                        <h4>Julie L. Arsenault</h4>
+                                        <p className="text-muted">@Programmer <span>| </span><span><Link to="#" className="text-pink">websitename.com</Link></span></p>
+                                    </div>
+                                    <button type="button" className="btn btn-primary mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button>
+                                    <div className="mt-4">
+                                        <div className="row">
+                                        </div>
+                                    </div>
 
+                                </div>
+                            </div>
+                            {/* <!-- end col --> */}
+                            <div className="col-lg-4">
+                                <div className="text-center card-box">
+                                    <div className="member-card pt-2 pb-2">
+                                        <div className="thumb-lg member-thumb mx-auto"><img src="https://bootdey.com/img/Content/avatar/avatar4.png" className="rounded-circle img-thumbnail" alt="profile-image" />
+                                        </div>
+                                        <div className="">
+                                            <h4>Christopher Gallardo</h4>
+                                            <p className="text-muted">@Webdesigner <span>| </span><span><Link to="#" className="text-pink">websitename.com</Link></span></p>
+                                        </div>
+                                        <button type="button" className="btn btn-primary mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button>
+                                        <div className="mt-4">
+                                            <div className="row">
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                {/* <!-- end col --> */}
+                            </div>
+                            {/* <!-- end row --> */}
+
+                            {/* <!-- end col --> */}
+                        </div>
+                        {/* <!-- end row --> */}
+
+                        {/* <!-- end row --> */}
+                    </div>
+                    {/* <!-- container --> */}
+                </div>
+
+
+
+            </div>
         </div>
     );
 };
--- client/src/JoinSignIn/NavBar.js
+++ client/src/JoinSignIn/NavBar.js
@@ -4,55 +4,55 @@ import './NavBar.css';
 import logo from '../images/WishListGift.png'
 
 
-/* function NavBar(props) {
-    return (
-        <div className="nav">
-            <div className="nav-container">
-                <div className='nav-home'>
-                    <Link to='/' className='nav-link'>
-                        <img id="logo" src={logo} alt="WishListGift - Home" />
-                    </Link>
-                </div>
+const NavBar = (props) => {
+    // return (
+    //     <div className="nav">
+    //         <div className="nav-container">
+    //             <div className='nav-home'>
+    //                 <Link to='/' className='nav-link'>
+    //                     <img id="logo" src={logo} alt="WishListGift - Home" />
+    //                 </Link>
+    //             </div>
 
 
-                <div className="nav-links">
-                    <Link to="/join-sign-in" className='nav-link'>Join/Sign In</Link>
-                    <Link to="/friends" className='nav-link'>Friends</Link>
-                </div>
-            </div>
-        </div>
-    );
-}
- */
-const state = () => {
-    showCollapsedMenu: false
-};
-const toggleMenu = () => {
-    this.setState({
-        showCollapsedMenu: !this.state.showCollapsedMenu
-      })
-}
+    //             <div className="nav-links">
+    //                 <Link to="/join-sign-in" className='nav-link'>Join/Sign In</Link>
+    //                 <Link to="/friends" className='nav-link'>Friends</Link>
+    //             </div>
+    //         </div>
+    //     </div>
+    // );
 
-const show = () => {
-    (this.state.showCollapsedMenu) ? "show" : "" ;}
-    
- function NavBar(props) {
+
+    // const state = () => {
+    //     showCollapsedMenu: false
+    // };
+    // const toggleMenu = () => {
+    //     this.setState({
+    //         showCollapsedMenu: !this.state.showCollapsedMenu
+    //       })
+    // }
+
+    // const show = () => {
+    //     (this.state.showCollapsedMenu) ? "show" : "" ;}
+
+    //  function NavBar(props) {
 
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <Link to ='/' className="navbar-brand"><img id="logo" src={logo} alt="WishListGift"></img></Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                <Link to='/' className="navbar-brand"><img id="logo" src={logo} alt="WishListGift"></img></Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                <div className={"collapse navbar-collapse" + show}  id="navbarNav">
+                <div className={"collapse navbar-collapse"} id="navbarNav">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <Link to ='/join-sign-in' className="nav-link">Home <span className="sr-only">(current)</span></Link>
+                            <Link to='/join-sign-in' className="nav-link">Home <span className="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
-                            <Link to ='/friends' className="nav-link">Friends</Link>
+                            <Link to='/friends' className="nav-link">Friends</Link>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link disabled" href="#">Disabled</a>
@@ -62,5 +62,5 @@ const show = () => {
             </nav>
         </div>
     );
- }
+}
 export default NavBar;
