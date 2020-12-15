241a7dba454ca6ba48c1607316536cb319fef519
Darys Bada <badadarys@gmail.com>
2020-12-15 15:28:28 -0500

    SignUp-Revised

--- client/src/SignUp/SignUp.js
+++ client/src/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import './SignUp.css';
 
 const SignUp = () => {
@@ -13,8 +13,6 @@ const SignUp = () => {
 
                     <label for="email">Email </label>
                     <input type="email" placeholder="Enter Email" name="email" required></input>
-                    <br />
-                    <br />
                     <label for="password">Create Password </label>
                     <input type="password" placeholder="Enter Password" name="password" required></input>
 
@@ -30,4 +28,46 @@ const SignUp = () => {
     );
 };
 
-export default SignUp;
+export default SignUp; */
+
+import React, { Component } from "react";
+import './SignUp.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import signUpBg from '../images/SignUpBG.png'
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div className='container fluid '>
+                <div className="row sign-up-form bg">        
+                    <form className="col-12 col-sm-4 sign-up-group">
+                        <h3>Sign Up</h3>
+                        <div className="form-group ">
+                            <label>First name</label>
+                            <input type="text" className="form-control" placeholder="First name" />
+                        </div>
+                        
+                        <div className="form-group">
+                            <label>Last name</label>
+                            <input type="text" className="form-control" placeholder="Last name" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input type="email" className="form-control" placeholder="Enter email" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" className="form-control" placeholder="Enter password" />
+                        </div>
+
+                        <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                        <br></br>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
